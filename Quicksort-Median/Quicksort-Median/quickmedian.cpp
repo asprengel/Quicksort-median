616eb7872299b265fa1ad3dec7ffe583dd647015
T1bolus <33393517+T1bolus@users.noreply.github.com>
2017-11-06 17:59:04 +0100

    Update quickmedian.cpp
    jetzt gehts echt

--- Quicksort-Median/Quicksort-Median/quickmedian.cpp
+++ Quicksort-Median/Quicksort-Median/quickmedian.cpp
@@ -43,7 +43,7 @@ int spalte_auf(int *a, int n1, int n2)
 
 int finde_ktes(int *a, int n0, int n, int k)
 {
-	int n1 = n0, n2 = n;
+	int n1 = n0, n2 = n + 1;
 	int tind;
 	while (true)
 	{
@@ -63,15 +63,46 @@ int finde_ktes(int *a, int n0, int n, int k)
 }
 
 
-#define in int
-void quickSortrek(int *a, int n1, int n2)
-{
-	in length = (n2 - n1) + 1;
-	if (length <= 1) return;
 
-	int p = finde_ktes(a, n1, n1 + length, length / 2);
-	quickSortrek(a, n1, p - 1);
-	quickSortrek(a, p + 1, n2);
+void quickSort(int *a, int n1, int n2)
+{
+	int k = 0;
+	int li = n1; int re = n2;
+
+	int *kli = new int[n2 - n1];
+	int *kre = new int[n2 - n1];
+
+	while(( li < re) || (k > 0))
+	{
+		if (!(li < re))
+		{
+			li = kli[k];
+			re = kre[k];
+			k--;
+		}
+		int length = (re - li) + 1;
+		int s = finde_ktes(a, li, li + length - 1, length / 2);
+		if ((re - li) >= 2)
+		{
+			if ((s - li) > (re - 2))
+			{
+				k = k + 1;
+				kli[k] = li;
+				kre[k] = s - 1;
+				li = s + 1;
+			}
+			else
+			{
+				k = k + 1;
+				kli[k] = s + 1;
+				kre[k] = re;
+				re = s - 1;
+			}
+		}
+		else
+			li = re;
+
+	}
 }
 
 
@@ -79,11 +110,11 @@ void quickSortrek(int *a, int n1, int n2)
 int main()
 {
 	int zahl;
-	int length;
+	unsigned int length;
 
 	cout << "Filename: ";
-	string filename = "test.txt";
-	cin >> filename;
+	string filename = "sample1_no_dups.txt";
+	//cin >> filename;
 
 	ifstream f(filename);
 	f >> length;
@@ -98,11 +129,11 @@ int main()
 	cout << "Eingelesen: " << index << endl;
 
 	int ende, start = clock();
-	quickSortrek(a, 0, length - 1);
+	quickSort(a, 0, length - 1);
 	ende = ((clock() - start) * 1000) / CLOCKS_PER_SEC;
 
 	ofstream output("output.txt");
-	for (size_t i = 0; i < length; i++)
+	for (size_t i = 0; i < length+1; i++)
 	{
 		output << a[i] << endl;
 	}
